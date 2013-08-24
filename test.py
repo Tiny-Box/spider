bccd15fde75b504e4917a70fb5d932440b8893b5
airfan13 <2820655657@qq.com>
2013-08-24 13:52:33 +0800

    improve something

--- test.py
+++ test.py
@@ -1,17 +1,27 @@
+#-*- coding:utf-8
 import BeautifulSoup 
 import requests
+from BeautifulSoup import BeautifulSoup
 
 url = 'http://tieba.baidu.com/f?kw=2011superfive&fr=index&ie=utf-8'
 
 pageSource = requests.get(url)
 
+page = []
+page = pageSource.text
+page.split(',')
+#print page
+
+doc = ['<html><head><title>PythonClub.org</title></head>',
+       '<body><p id="firstpara" align="center">This is paragraph <b>one</b> of ptyhonclub.org.',
+       '<p id="secondpara" align="blah">This is paragraph <b>two</b> of pythonclub.org.',
+       '</html>']
+soup = BeautifulSoup(''.join(page))
+print soup.html.head.title
 hrefs = []
-soup = BeautifulSoup(pageSource)
-results = soup.find_all('a', href = True)
+results = soup.findAll('a', href = True)
 for a in results:
 	href = a.get('href').encode('utf-8')
-	if not href.startswith('http'):
-		href = urljoin(url, href)
 	hrefs.append(href)
 
 print hrefs
